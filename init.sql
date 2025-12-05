327521329ecc7de528c85bcbf2d53e86d136f0bb
CauePrado <151967357+CauePrad0@users.noreply.github.com>
2025-12-05 15:11:49 -0300

    .env e gitgnore

--- init.sql
+++
@@ -1,8 +0,0 @@
--- Cria o usuário
-CREATE USER ironpump_user WITH PASSWORD 'iron_secret_2024';
-
--- Dá superpoderes (em dev é mais fácil que ficar dando GRANT um por um)
-ALTER USER ironpump_user WITH SUPERUSER;
-
--- Cria o banco com esse dono
-CREATE DATABASE ironpump OWNER ironpump_user;
